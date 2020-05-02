56dfcdf68b689d9ce3ddc4a435854ec64125e360
kevene1996 <kevin@curlybrackets.com.lb>
2020-05-03 07:29:49 +0800

    angular update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDRZJ8Q90d75ptBNG_5wLoMXJgnJKVdWzw',
+    authDomain: 'eljumper-f9478.firebaseapp.com',
+    databaseURL: 'https://eljumper-f9478.firebaseio.com',
+    projectId: 'eljumper-f9478',
+    storageBucket: 'eljumper-f9478.appspot.com',
+    messagingSenderId: '391256126764',
+    appId: '1:391256126764:web:e9572a3d61171266902889'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDRZJ8Q90d75ptBNG_5wLoMXJgnJKVdWzw',
+    authDomain: 'eljumper-f9478.firebaseapp.com',
+    databaseURL: 'https://eljumper-f9478.firebaseio.com',
+    projectId: 'eljumper-f9478',
+    storageBucket: 'eljumper-f9478.appspot.com',
+    messagingSenderId: '391256126764',
+    appId: '1:391256126764:web:e9572a3d61171266902889'
+  }
 };
 
 /*
